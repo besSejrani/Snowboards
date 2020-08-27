3f6374dd316e9d46d131d71b6666073b991ff681
Bes <besjan.sejrani@outlook.fr>
2020-08-28 01:39:07 +0200

    added 404 page

--- public/index.php
+++ public/index.php
@@ -18,6 +18,8 @@ switch ($uri) {
     case "/snow_update":
         require_once(dirname(__DIR__) . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "snow_update.php");
         break;
+    default:
+        require_once(dirname(__DIR__) . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "pages" . DIRECTORY_SEPARATOR . "404.php");
 }
 
 require_once(dirname(__DIR__) . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "layout" . DIRECTORY_SEPARATOR . "footer.php");
---
+++ src/pages/404.php
@@ -0,0 +1,3 @@
+<div class="vh-100 d-flex justify-content-center align-items-center">
+    <h1>Sorry, 404 Error</h1>
+</div>
