4f7e1e94d4c144a4fa80c84556cbc508625f3d6e
SEJRANI Besjan <Besjan.SEJRANI@cpnv.ch>
2020-08-27 16:02:08 +0200

    added some structure

--- src/layout/header.php
+++ src/layout/header.php
@@ -8,5 +8,14 @@
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <title>Document</title>
 </head>
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand"><img
+            src="<?php dirname(__DIR__) . DIRECTORY_SEPARATOR . "assets" . DIRECTORY_SEPARATOR . "img" . DIRECTORY_SEPARATOR . "docker-3.svg" ?>"
+            alt="bootstrap logo"></a>
+    <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+</nav>
 
 <body>
