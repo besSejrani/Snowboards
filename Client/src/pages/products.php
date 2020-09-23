107db81f269fec0cd65cada39addaad331a4c69b
Bes Sejrani <besjan.sejrani@outlook.fr>
2020-09-23 16:55:49 +0200

    add select to product page

--- Client/src/pages/products.php
+++ Client/src/pages/products.php
@@ -11,10 +11,23 @@ $title = "Snowboards | Products";
     <div class="container px-0">
         <div class="d-flex justify-content-between align-items-center my-4 ">
             <h1>Products</h1>
-            <a href="/snow_add">
-                <button type="button" class="btn btn-primary">Add Snow</button>
-            </a>
+
+            <div class="d-flex">
+                <a class="mx-3" href="/snow_add">
+                    <button type="button" class="btn btn-primary">Add Snow</button>
+                </a>
+
+                <div>
+                    <select class="custom-select" id="inputGroupSelect01">
+                        <option selected>Selection</option>
+                        <option value="1">User</option>
+                        <option value="2">Snows</option>
+                        <option value="3">Events</option>
+                    </select>
+                </div>
+            </div>
         </div>
+
     </div>
 
     <?php
@@ -79,6 +92,16 @@ EOT;
 </div>
 
 <?php
-$js = '';
+$js = '
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+    integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+</script>
+';
 $content = ob_get_clean();
 require(dirname(__DIR__)  . DIRECTORY_SEPARATOR . "layout" . DIRECTORY_SEPARATOR . "layout.php");
