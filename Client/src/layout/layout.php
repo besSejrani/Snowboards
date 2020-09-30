7c7b6d3eaf5f297ee185fc1754dda96d30bac094
Bes Sejrani <besjan.sejrani@outlook.fr>
2020-09-30 16:09:09 +0200

    changed product page

--- Client/src/layout/layout.php
+++ Client/src/layout/layout.php
@@ -4,17 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
     <link rel="stylesheet" href="./scss/index.css">
     <title><?= $title ?? "Snowboard" ?></title>
 </head>
 
 
 <nav class="navbar navbar-light bg-light fixed-top d-flex justify-content-between" id="nav">
-    <a href="/" class="navbar-brand"><img
-            src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-            alt="bootstrap logo" height="40"></a>
+    <a href="/" class="navbar-brand"><img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="bootstrap logo" height="40"></a>
     <div class="d-flex justify-content-between ">
         <form class="form-inline mr-5">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
@@ -23,14 +21,14 @@
         <div class="d-flex flex-row">
             <a class="nav-link active p-2" href="/">Home <span class="sr-only"></span></a>
             <a class="nav-link active p-2" href="/events">Events <span class="sr-only"></span></a>
+            <a class="nav-link active p-2" href="/contact">Contact <span class="sr-only"></span></a>
 
             <?php if (!isset($_SESSION['role'])) : ?>
-            <a class="nav-link active p-2" href="/login">Login<span class="sr-only"></span></a>
+                <a class="nav-link active p-2" href="/login">Login<span class="sr-only"></span></a>
             <?php else : ?>
-            <a class="nav-link active p-2" href="/products">Products<span class="sr-only"></span></a>
-            <a class="nav-link active p-2" href="http://localhost:4000" target="_blank">Dashboard <span
-                    class="sr-only"></span></a>
-            <a class="nav-link active p-2" href="/logout">Logout<span class="sr-only"></span></a>
+                <a class="nav-link active p-2" href="/products">Products<span class="sr-only"></span></a>
+                <a class="nav-link active p-2" href="http://localhost:4000" target="_blank">Dashboard <span class="sr-only"></span></a>
+                <a class="nav-link active p-2" href="/logout">Logout<span class="sr-only"></span></a>
             <?php endif; ?>
 
         </div>
@@ -46,7 +44,6 @@
     </footer>
     <?= $js ?>
     <script src="./js/utils/scrollShowUp.js"></script>
-
 </body>
 
 </html>
---
+++ Client/src/pages/contact.php
@@ -0,0 +1,20 @@
+<?php
+ob_start();
+$title = "Snowboards | Admin";
+?>
+
+<div class="vh-100 d-flex justify-content-center align-items-center">
+    <h1>Contact</h1>
+</div>
+
+
+
+<?php
+
+$js = '<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+crossorigin=""></script>
+';
+
+$content = ob_get_clean();
+require(dirname(__DIR__)  . DIRECTORY_SEPARATOR . "layout" . DIRECTORY_SEPARATOR . "layout.php");
